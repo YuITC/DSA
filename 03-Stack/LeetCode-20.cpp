e790cd5d02d03cd755cdfffbfd0e1d80d460c10b
Nguyen Phu Tai <111862377+YuITC@users.noreply.github.com>
2023-08-27 21:03:34 +0700

    Create LeetCode-20.cpp

---
+++ 03-Stack/LeetCode-20.cpp
@@ -0,0 +1,26 @@
+// 20. Valid Parentheses
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for (char x : s) 
+        {
+            if (x == '(' || 
+                x == '[' || 
+                x == '{') 
+                st.push(x);
+
+            else
+            {
+                if (st.empty() || 
+                    (x == ')' && st.top() != '(') ||
+                    (x == ']' && st.top() != '[') ||
+                    (x == '}' && st.top() != '{') )
+                    return false;
+                
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
+};
