1263afb2a736a4a5f8759f83132497c424a76bac
Nguyen Phu Tai <111862377+YuITC@users.noreply.github.com>
2023-08-27 16:55:31 +0700

    Delete 02-Static-Array directory

--- 02-Static-Array/LeetCode-27.py
+++
@@ -1,9 +0,0 @@
-# 27. Remove Element
-class Solution:
-    def removeElement(self, nums: List[int], val: int) -> int:
-        k = 0
-        for x in nums:
-            if x != val:
-                nums[k] = x
-                k += 1
-        return k
