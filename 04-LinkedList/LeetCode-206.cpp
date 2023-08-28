4806cde0f7f5db7187359603e537d8094b28f5f5
Nguyen Phu Tai <111862377+YuITC@users.noreply.github.com>
2023-08-28 10:40:50 +0700

    Create LeetCode-206.cpp

---
+++ 04-LinkedList/LeetCode-206.cpp
@@ -0,0 +1,28 @@
+// 206. Reverse Linked List
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prv = NULL;
+        ListNode* cur = head;
+        ListNode* nxt;
+
+        while (cur)
+        {
+            nxt = cur->next;
+            cur->next = prv;
+            prv = cur;
+            cur = nxt;
+        }
+        return prv;
+    }
+};
