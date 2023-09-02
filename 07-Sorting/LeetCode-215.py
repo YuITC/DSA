c35c31959dd398486c221cd48f478787412191d9
Nguyen Phu Tai <111862377+YuITC@users.noreply.github.com>
2023-09-02 10:18:26 +0700

    Update LeetCode-215.py

--- 07-Sorting/LeetCode-215.py
+++ 07-Sorting/LeetCode-215.py
@@ -1,19 +1,20 @@
 # 215. Kth Largest Element in an Array
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        k -= 1
         def quickSelect(l, r):
+            pi = random.randint(l, r)
+            nums[pi], nums[r] = nums[r], nums[pi]
             pivot = nums[r]
-            p = l
+            p = lz
             for i in range(l, r):
-                if nums[i] >= pivot:
+                if nums[i] > pivot:
                     nums[i], nums[p] = nums[p], nums[i]
                     p += 1
             nums[p], nums[r] = nums[r], nums[p]
 
-            if p > k:
+            if p > k - 1:
                 return quickSelect(l, p - 1)
-            elif p < k:
+            elif p < k - 1:
                 return quickSelect(p + 1, r)
             else:
                 return nums[p]
